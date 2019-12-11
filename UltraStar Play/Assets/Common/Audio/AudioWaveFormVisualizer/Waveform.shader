d807eae113263ba5d54daf4681ecfff819a1300a
Andreas Stange <anst.dev@gmail.com>
2019-12-11 17:15:21 +0100

    experimented with shader for audio waveform creation

---
+++ UltraStar Play/Assets/Common/Audio/AudioWaveFormVisualizer/Waveform.shader
@@ -0,0 +1,56 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Unlit/Waveform"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags {"RendererType" = "Opaque"}
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            float _samples[1023];
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+            
+            fixed4 frag(v2f i) : SV_Target
+            {
+                int index = i.uv.x * 1023;
+				float s = _samples[index];
+				if (abs(i.uv.y - s) < 0.01f)
+					return fixed4(1, 1, 1, 1);
+				else
+					return fixed4(0, 0, 0, 1);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ UltraStar Play/Assets/Scenes/SongEditor/Waveform.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Waveform : MonoBehaviour
+{
+    private RectTransform rectTransform;
+    private Texture2D texture;
+    private RawImage rawImage;
+    private Material material;
+    private float[] samples = new float[1023];
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        rawImage = GetComponent<RawImage>();
+
+        material = rawImage.material;
+        if (material == null)
+        {
+            Debug.LogError("no material");
+        }
+
+        for (int i = 0; i < samples.Length; i++)
+        {
+            samples[i] = (float)i / samples.Length;
+            // Debug.Log(samples[i]);
+        }
+        material.SetFloatArray("_samples", samples);
+    }
+
+    void Update()
+    {
+        // for (int i = 0; i < samples.Length; i++)
+        // {
+        //     samples[i] = Mathf.Sin(Time.time + i * 1f) / 2 + 0.5f;
+        // }
+        // material.SetFloatArray("_samples", samples);
+    }
+
+    private void CreateTexture()
+    {
+        int textureWidth = (int)rectTransform.rect.width;
+        int textureHeight = (int)rectTransform.rect.height;
+
+        // create the texture and assign to the guiTexture: 
+        texture = new Texture2D(textureWidth, textureHeight);
+        rawImage.texture = texture;
+    }
+}
